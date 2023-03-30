62de8d1643e6f2b90b3292a9662bcb9fde8e7b01
NexWan <l21051403@saltillo.tecnm.mx>
2023-03-30 01:14:03 -0600

    PAIN

--- Unidad3/Computadoras/js/script.js
+++ Unidad3/Computadoras/js/script.js
@@ -48,7 +48,35 @@ window.onload = () =>{
         sumOpacity();
     }
     writeText()
+
+    let showCpuP = document.getElementById('cpu')
+
+    showCpuP.addEventListener('click', function cpuMsg(){
+        let msg = 'A CPU, or central processing unit, is a type of electronic circuitry that serves as the "brain" of a computer or other digital device. It is responsible for executing instructions that make up software programs, performing calculations, and managing the flow of data within a system.\n' +
+            'CPUs come in a variety of different architectures, speeds, and power consumption levels, and are typically identified by their manufacturer and model number. They may also include features like multiple cores or threads, which allow them to perform multiple tasks simultaneously, and specialized instruction sets for specific types of computations.\n' +
+            'The performance of a CPU can have a significant impact on the overall speed and responsiveness of a computer or device, as well as its ability to handle complex tasks like gaming or video editing. As a result, CPUs are a critical component of modern computing systems, and their development and evolution are closely watched by computer enthusiasts, engineers, and researchers around the world.    '
+        i = 0;
+        if(showCpuP.dataset.clicked === 'false'){
+            showCpuP.dataset.clicked = 'true';
+            showMsg()
+            disableButton(showCpuP, 'click',`cpuMsg`)
+            function showMsg(){
+                if(i < msg.length){
+                    document.getElementById('cpuP').innerHTML += msg[i]
+                    i++
+                    setTimeout(showMsg,2)
+                }
+            }
+        }
+        function disableButton(button, trigger, func){
+            console.log(trigger + ' ' + func)
+            button.removeEventListener('click', cpuMsg,  false);
+        }
+    })
+
 }
+
+
 /*Here ends the code of JS for the page index*/
 
 /*Here starts the JS code of the page processors*/
